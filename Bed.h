64cc5cf321e155560edf67755010f0500f6e439f
dilarasen1 <sen1@kenyon.edu>
2024-09-14 14:40:45 -0400

    Updated Bed.h

--- Bed.h
+++ Bed.h
@@ -3,7 +3,6 @@
 class Bed : public Furniture {
 private:
     // Attributes specific to Bed class
-    int numOfSeats;//delete this
     std::string bedSize;            // Size of the bed
     std::string bedFrameMaterial;   // Material that the frame is made out of
     std::string bedStyle;           // Style of the bed
@@ -11,12 +10,33 @@ private:
     std::string bedColor;           // Color of the bed frame
 
 public:
-    Bed(std::string n, double p, int q, int seats)
-        : Furniture(n, p, q), numOfSeats(seats) {}
-    int getNumOfSeats() const { return numOfSeats; }
-    void setNumOfSeats(int seats) { numOfSeats = seats; }
+    // Constructor
+    Bed(std::string n, double p, int q, std::string size, std::string frameMaterial, std::string style, bool storage, std::string color)
+        : Furniture(n, p, q), bedSize(size), bedFrameMaterial(frameMaterial), bedStyle(style), bedStorage(storage), bedColor(color) {}
+    
+    // Getter and setter methods
+    std::string getBedSize() const { return bedSize; }
+    void setBedSize(const std::string size) { bedSize = size; }
+
+    std::string getBedFrameMaterial() const { return bedFrameMaterial; }
+    void setBedFrameMaterial(const std::string frameMaterial) { bedFrameMaterial = frameMaterial; }
+
+    std::string getBedStyle() const { return bedStyle; }
+    void setBedStyle(const std::string style) { bedStyle = style; }
+
+    bool getBedStorage() const { return bedStorage; }
+    void setBedStorage(bool storage) { bedStorage = storage; }
+
+    std::string getBedColor() const { return bedColor; }
+    void setBedColor(const std::string color) { bedColor = color; }
+
+    // Display method
     void display() const override {
         Furniture::display();
-        std::cout << "Number of Seats: " << numOfSeats << std::endl;
+        std::cout << "Number of Seats: " << bedSize << std::endl;
+        std::cout << "Number of Seats: " << bedFrameMaterial << std::endl;
+        std::cout << "Number of Seats: " << bedStyle << std::endl;
+        std::cout << "Number of Seats: " << bedStorage << std::endl;
+        std::cout << "Number of Seats: " << bedColor << std::endl;
     }
 };
