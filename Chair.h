e95965d8936bb3efe16a6589519dfe2d0ed50788
juansergiomatabuena <sergiomatabuena1@kenyon.edu>
2024-09-15 17:26:21 -0400

    Updated Chair.h

--- Chair.h
+++ Chair.h
@@ -2,17 +2,36 @@
 
 class Chair : public Furniture {
 private:
-    bool hasArmrest;
+    // Attributes specific to Chair class
+    bool hasArmrest; // Does the bed have armrests?
+    bool doesRecline; // Does the chair recline?
+    std::string chairMaterial; // Material the chair is made out of
+    int chairNumOfLegs; // Number of legs the chair has
 
 public:
-    Chair(std::string n, double p, int q, bool armrest)
-        : Furniture(n, p, q), hasArmrest(armrest) {}
+    // Constructor
+    Chair(std::string n, double p, int q, bool armrest, bool recline, std::string material, int numLegs)
+        : Furniture(n, p, q), hasArmrest(armrest), doesRecline(recline), chairMaterial(material), chairNumOfLegs(numLegs) {}
 
+    //Getter and setter methods
     bool getHasArmrest() const { return hasArmrest; }
     void setHasArmrest(bool armrest) { hasArmrest = armrest; }
 
+    bool getDoesRecline() const { return doesRecline; }
+    void setDoesRecline(bool recline) { doesRecline = recline; }
+
+    std::string getChairMaterial() const { return chairMaterial; }
+    void setChairMaterial(const std::string material) { chairMaterial = material; }
+
+    int getChairNumOfLegs() const { return chairNumOfLegs; }
+    void setChairNumOfLegs(int numLegs) { chairNumOfLegs = numLegs; }
+
+    // Display method
     void display() const override {
         Furniture::display();  // Display inherited attributes
-        std::cout << "Armrest: " << (hasArmrest ? "Yes" : "No") << std::endl;
+        std::cout << "Does the chair have an armrest: " << (hasArmrest ? "Yes" : "No") << std::endl;
+        std::cout << "Does the chair recline: " << (doesRecline ? "Yes" : "No") << std::endl;
+        std::cout << "Chair material: " << chairMaterial << std::endl;
+        std::cout << "Number of chair legs: " << chairNumOfLegs << std::endl;
     }
 };
